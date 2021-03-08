bed1aad8af633c598553ea61c14ae5887f31cbc1
CorentinCastel <corentin.castel0@gmail.com>
2021-03-08 15:59:45 +0100

    fin de séance : import bdd + models + maincontroller + auth

---
+++ app/controllers/MyAuth.php
@@ -0,0 +1,55 @@
+<?php
+namespace controllers;
+use models\User;
+use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\UResponse;
+use Ubiquity\utils\http\USession;
+use Ubiquity\utils\http\URequest;
+use Ubiquity\attributes\items\router\Route;
+
+#[Route(path: "/login",name: "login", inherited: true,automated: true)]
+class MyAuth extends \Ubiquity\controllers\auth\AuthController{
+
+	protected function onConnect($connected) {
+		$urlParts=$this->getOriginalURL();
+		USession::set($this->_getUserSessionKey(), $connected);
+		if(isset($urlParts)){
+			$this->_forward(implode("/",$urlParts));
+		}else{
+			UResponse::header('location','/');
+		}
+	}
+
+	protected function _connect() {
+		if(URequest::isPost()){
+			$email=URequest::post($this->_getLoginInputName());
+			$password=URequest::post($this->_getPasswordInputName());
+            if($email!= null && $password!=null){
+                $user=DAO::getOne(User::class, 'email= ?',false, [$email]);
+                if(isset($user)){
+                    if($user->getPassword() == $password){
+                        USession::set('idUser', $user->getId());
+                        return $user;
+                    }
+                }
+
+            }
+        }
+		return null;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see \Ubiquity\controllers\auth\AuthController::isValidUser()
+	 */
+	public function _isValidUser($action=null) {
+		return USession::exists($this->_getUserSessionKey());
+	}
+
+	public function _getBaseRoute() {
+		return '/MyAuth';
+	}
+	
+
+
+}
