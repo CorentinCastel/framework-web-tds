f770680a8d512deddcdaaaf98418c73c017b64bc
CorentinCastel <corentin.castel0@gmail.com>
2021-03-22 15:36:31 +0100

    Ajout de la page store + page section + page produit (non terminées)

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -4,6 +4,7 @@ namespace controllers;
  use models\Basket;
  use models\Order;
  use models\Product;
+ use models\Section;
  use Ubiquity\attributes\items\router\Route;
  use Ubiquity\controllers\auth\AuthController;
  use Ubiquity\controllers\auth\WithAuthTrait;
@@ -20,9 +21,9 @@ class MainController extends ControllerBase{
     #[Route('_default', name:'home')]
     public function index(){
         $nbCommande = DAO::count(Order::class, 'idUser=?', [USession::get("idUser")]);
-        $products = DAO::getAll(Product::class, 'promotion<0');
+        $productsPromo = DAO::getAll(Product::class, 'promotion<0');
         $nbPaniers = DAO::count(Basket::class, 'idUser=?', [USession::get("idUser")]);
-        $this->jquery->renderView('MainController/index.html', ['nbCommandes'=>$nbCommande, 'nbPaniers'=>$nbPaniers, 'products'=>$products]);
+        $this->jquery->renderView('MainController/index.html', ['nbCommandes'=>$nbCommande, 'nbPaniers'=>$nbPaniers, 'products'=>$productsPromo]);
     }
 
 
@@ -32,6 +33,26 @@ class MainController extends ControllerBase{
         return new MyAuth($this);
     }
 
+    #[Route('store', name: 'store')]
+    public function store(){
+        $productsPromo = DAO::getAll(Product::class, 'promotion<0');
+        $sections = DAO::getAll(Section::class);
+        $this->jquery->renderView('mainController/store.html', ['products'=>$productsPromo, "sections"=>$sections]);
+    }
 
+    #[Route('section/{section_id}', name: 'section')]
+    public function section($section_id){
+        $products = DAO::getAll(Product::class, 'idSection='.$section_id);
+        $sections = DAO::getAll(Section::class);
+        $nomSect = DAO::getOne(Section::class, 'id='.$section_id);
+        $this->jquery->renderView('mainController/section.html', ['sections'=>$sections, 'products'=>$products, 'sectionName'=>$nomSect]);
+    }
+
+    #[Route('product/{section_id}/{product_id}', name: 'product')]
+    public function product($section_id, $product_id){
+        $product = DAO::getAll(Product::class, 'id='.$product_id);
+        $sections = DAO::getAll(Section::class);
+        $this->jquery->renderView('mainController/product.html', ['sections'=>$sections, 'product'=>$product]);
+    }
 
 }
